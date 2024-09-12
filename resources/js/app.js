cdfa8d34eaf7892b3588c1e3ec8d6c4a0344ae91
Alexander Pecherskiy <apecherskiy@yandex.ru>
2024-09-12 20:21:25 +0700

    config quasar

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,19 +5,39 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { Quasar } from 'quasar'
+import quasarOptions from './quasar_options';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+const appName = import.meta.env.VITE_APP_NAME || 'Test app';
+window.TWA = window.Telegram ? window.Telegram.WebApp : null;
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        const app = createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .mount(el);
+            .use(Quasar, quasarOptions)
+        app.config.globalProperties.TWA = window.TWA
+        return app.mount(el);
     },
     progress: {
         color: '#4B5563',
     },
 });
+
+const currentLang = {
+    'comming_soon': 'Comming soon'
+}
+
+
+window.t = (key) => {
+    return currentLang[key] || key;
+}
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -16,5 +18,16 @@ export default defineConfig({
                 },
             },
         }),
+        quasar({
+            sassVariables: 'resources/js/quasar-variables.sass'
+        })
     ],
+    resolve: {
+        alias: {
+          '@shared': path.resolve(__dirname, './resources/js/shared/'),
+          '@composables': path.resolve(__dirname, './resources/js/composables'),
+          '@components': path.resolve(__dirname, './resources/js/components'),
+          '@pages': path.resolve(__dirname, './resources/js/pages'),
+        },
+    }
 });
