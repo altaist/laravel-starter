cced02b5898c8d7cbff664a735a48b46c24cf451
Alexander Pecherskiy <apecherskiy@yandex.ru>
2024-09-13 10:29:16 +0700

    add morphs to service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Relation::enforceMorphMap([
+            'user' => User::class
+        ]);
+
+        Validator::extend('poly_exists', function ($attribute, $value, $parameters, $validator) {
+            if (!$objectType = data_get($validator->getData(), $parameters[0], false)) {
+                return false;
+            }
+
+            return !empty(resolve(Relation::getMorphedModel($objectType))->find($value));
+        }, "Wrong polymorphic object id");
     }
 }
