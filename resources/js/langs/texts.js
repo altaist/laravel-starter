65302229d30c6ae01e051d9596ef81a0b262a5f0
Alexander Pecherskiy <apecherskiy@yandex.ru>
2024-09-25 08:33:10 +0700

    add locales

---
+++ resources/js/langs/texts.js
@@ -0,0 +1,37 @@
+
+export default {
+    'main.title': {
+        'ru-Ru': 'Заголовок',
+        'en': 'Title'
+    },
+    'header.btn_profile': {
+        'ru-Ru': 'Профиль',
+        'en': 'Profile'
+    },
+    'header.btn_langs': {
+        'ru-Ru': 'Языки',
+        'en': 'Langs'
+    },
+    'header.btn_wallet': {
+        'ru-Ru': 'Кошелек',
+        'en': 'Wallet'
+    },
+    'footer.btn_code': {
+        'ru-Ru': 'Код',
+        'en': 'Code'
+    },
+    'footer.btn_social': {
+        'ru-Ru': 'Поделись',
+        'en': 'Links'
+    },
+    'footer.btn_share': {
+        'ru-Ru': 'Пригласи друга',
+        'en': 'Share'
+    },
+    'langs.select_lang': {
+        'ru-Ru': 'Выбрать язык',
+        'en': 'Change language'
+    },
+
+
+}
