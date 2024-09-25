2b9ee40a63b1f6eb53d30692c094a671cb60e056
Alexander Pecherskiy <apecherskiy@yandex.ru>
2024-09-25 08:38:52 +0700

    new components for main screen

--- resources/js/components/shared/Footer.vue
+++ resources/js/components/shared/Footer.vue
@@ -1,5 +1,13 @@
 <template>
-    {{ label }}
+    <div style="position: fixed; bottom: 0; width: 100%;">
+        <div class="q-pa-md">
+            <div class="row">
+                <div class="col"><btn :label="t('footer.btn_code')"/></div>
+                <div class="col text-center"><btn :label="t('footer.btn_social')"/></div>
+                <div class="col text-right"><btn :label="t('footer.btn_share')"/></div>
+            </div>
+        </div>
+    </div>
 </template>
 <script setup>
 const props = defineProps({
